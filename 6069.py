981215433dba170bb76662788f3bebca179affa3
tmfrl99 <93257321+tmfrl99@users.noreply.github.com>
2022-10-01 23:35:21 +0900

    codeUp

---
+++ 6069.py
@@ -0,0 +1,14 @@
+s = input()
+if s == "A":
+    print("best!!!")
+else:
+    if s == "B":
+        print("good!!")
+    else:
+        if s == "C":
+            print("run!")
+        else:
+            if s == "D":
+                print("slowly~")
+            else:
+                print("what?")
---
+++ 6070.py
@@ -0,0 +1,11 @@
+m = int(input())
+if 3 <= m <= 5:
+    print("spring")
+else:
+    if 6 <= m <= 8:
+        print("summer")
+    else:
+        if 9 <= m <= 11:
+            print("fall")
+        else:
+            print("winter")
